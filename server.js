e9e2df9722cb1a91a695ddbcb59dfdc14b16d18d
刘思伟 <swyy0412@gmail.com>
2016-01-30 21:01:55 +0800

    add the node server

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.use(express.static('public'));
+
+app.set('port', process.env.PORT || 3000);
+var server = http.listen(app.get('port'), function() {
+    console.log('start at port:' + server.address().port);
+});
